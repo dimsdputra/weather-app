1159c78580a55d2b4e6497c5e981fcdf1a1b4089
dimas dwi putra <dimsdputra@gmail.com>
2023-01-19 18:26:27 +0700

    weather app

---
+++ src/App.js
@@ -0,0 +1,268 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  IoMdSunny,
+  IoMdRainy,
+  IoMdCloudy,
+  IoMdSnow,
+  IoMdThunderstorm,
+  IoMdSearch,
+} from "react-icons/io";
+
+import {
+  BsCloudHaze2Fill,
+  BsCloudDrizzleFill,
+  BsEye,
+  BsWater,
+  BsThermometer,
+  BsWind,
+} from "react-icons/bs";
+
+import { TbTemperatureCelsius } from "react-icons/tb";
+import { ImSpinner8 } from "react-icons/im";
+
+//30f19104ac4a548f2102461d0dbaa1db
+const APIkey = "30f19104ac4a548f2102461d0dbaa1db";
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const [location, setLocation] = useState("Indonesia");
+  const [inputValue, setInputValue] = useState("");
+  const [animate, setAnimate] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState(false);
+
+  //handle input
+  const handleInput = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  //handle submit
+  const handleSubmit = (e) => {
+    if (inputValue !== "") {
+      setLocation(inputValue);
+    }
+
+    //select input
+    const input = document.querySelector("input");
+
+    //if input is empty
+    if (input.value === "") {
+      setAnimate(true);
+      setTimeout(() => {
+        setAnimate(false);
+      }, 500);
+    }
+
+    //clear input
+    input.value = "";
+
+    //get input
+    e.preventDefault();
+  };
+
+  //Fetch data
+  useEffect(() => {
+    //set Loading
+    setLoading(true);
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${APIkey}`;
+
+    axios
+      .get(url)
+      .then((res) => {
+        setTimeout(() => {
+          setData(res.data);
+          setLoading(false);
+        }, 1500);
+      })
+      .catch((err) => {
+        setLoading(false);
+        setErrorMsg(err);
+      });
+  }, [location]);
+
+  //error message
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setErrorMsg("");
+    }, 2000);
+    //clear timer
+    return () => clearTimeout(timer);
+  }, [errorMsg]);
+
+  // Loader animation
+  if (!data) {
+    return (
+      <div className="w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col justify-center items-center">
+        <div>
+          <ImSpinner8 className="text-5xl animate-spin text-white" />
+        </div>
+      </div>
+    );
+  }
+
+  //set icon according to the weather
+  let icon;
+
+  switch (data.weather[0].main) {
+    case "Clouds":
+      icon = <IoMdCloudy />;
+      break;
+    case "Haze":
+      icon = <BsCloudHaze2Fill className="text-[#6e777a]"/>;
+      break;
+    case "Rain":
+      icon = <IoMdRainy className="text-[#6e777a]"/>;
+      break;
+    case "Clear":
+      icon = <IoMdSunny className="text-[#ffde33]"/>;
+      break;
+    case "Drizzle":
+      icon = <BsCloudDrizzleFill className="text-[#6e777a]"/>;
+      break;
+    case "Snow":
+      icon = <IoMdSnow className="text-[#31cafb]"/>;
+      break;
+    case "Thunderstorm":
+      icon = <IoMdThunderstorm className="text-[#3b4346]"/>;
+      break;
+
+    default:
+      break;
+  }
+
+  //date object
+  const date = new Date();
+
+  return (
+    <div className="w-full h-screen bg-gradientBg bg-no-repeat bg-center bg-cover flex flex-col items-center justify-center px-4 lg:px-0">
+      {errorMsg && <div className="w-full max-w-[450px] lg:max-w-[450px] bg-[#ff208c] text-white absolute z-10 top-10 lg-top-10 p-4 capitalize rounded-md">{`${errorMsg.response.data.message}`}</div>}
+      {/* form */}
+      <form
+        className={`${
+          animate ? "animate-shake" : "animate-none"
+        } h-16 bg-black/30 w-full max-w-[450px] rounded-full backdrop-blur-[32px] mb-8`}
+      >
+        <div className="h-full relative flex items-center justify-between p-2">
+          <input
+            onChange={(e) => handleInput(e)}
+            className="flex-1 bg-transparent outline-none placeholder:text-white text-white text-[15px] font-light pl-6 h-full"
+            type="text"
+            placeholder="Search by city or country"
+          />
+          <button
+            onClick={(e) => handleSubmit(e)}
+            className="bg-[#0569d4] hover:bg-[#064384] w-20 h-12 rounded-full flex justify-center items-center transition"
+          >
+            <IoMdSearch className="text-2xl text-white" />
+          </button>
+        </div>
+      </form>
+
+      {/* card */}
+      <div className="w-full max-w-[450px] bg-black/20 min-h-[584px] text-white backdrop-blur-[32px] rounded-[32px] py-12 px-6">
+        {loading ? (
+          <div className="w-full h-full flex justify-center items-center">
+            <ImSpinner8 className="text-white text-5xl animate-spin" />
+          </div>
+        ) : (
+          <div>
+            {/* card top */}
+            <div className="flex items-center gap-x-5">
+              {/* icon */}
+              <div className="text-[87px]">{icon}</div>
+              <div>
+                {/* country name */}
+                <div className="text-2xl font-semibold">
+                  {data.name}, {data.sys.country}
+                </div>
+                {/* date */}
+                <div>
+                  {date.getUTCDate()}/{date.getUTCMonth() + 1}/
+                  {date.getUTCFullYear()}
+                </div>
+              </div>
+            </div>
+
+            {/* card body */}
+            <div className="my-20">
+              <div className="flex justify-center">
+                {/* temp */}
+                <div className="text-[144px] leading-none font-light">
+                  {parseInt(data.main.temp)}
+                </div>
+                {/* celcius icon */}
+                <div className="text-8xl">
+                  <TbTemperatureCelsius />
+                </div>
+              </div>
+              <div className="capitalize text-center">
+                {data.weather[0].description}
+              </div>
+            </div>
+
+            {/* card bottom */}
+            <div className="max-w-[378px] mx-auto flex flex-col gap-y-6">
+              <div className="flex justify-between">
+                {/* Visibility */}
+                <div className="flex items-center gap-x-2">
+                  {/* icon */}
+                  <div className="text-[20px]">
+                    <BsEye />
+                  </div>
+                  <div>
+                    Visibility{" "}
+                    <span className="ml-2">{data.visibility / 1000} km</span>
+                  </div>
+                </div>
+
+                {/* Thermometer */}
+                <div className="flex items-center gap-x-2">
+                  {/* icon */}
+                  <div className="text-[20px]">
+                    <BsThermometer />
+                  </div>
+                  <div className="flex">
+                    Feels like
+                    <div className="flex ml-2">
+                      {parseInt(data.main.feels_like)}
+                      <TbTemperatureCelsius />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex justify-between">
+                {/* Humidity */}
+                <div className="flex items-center gap-x-2">
+                  {/* icon */}
+                  <div className="text-[20px]">
+                    <BsWater />
+                  </div>
+                  <div>
+                    Humidity
+                    <span className="ml-2">{data.main.humidity} %</span>
+                  </div>
+                </div>
+
+                {/* Wind */}
+                <div className="flex items-center gap-x-2">
+                  {/* icon */}
+                  <div className="text-[20px]">
+                    <BsWind />
+                  </div>
+                  <div>
+                    Wind<span className="ml-2">{data.wind.speed} m/s</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
